5cc2369445154a128f226c1bc510d31565b82064
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-09-08 09:31:50 +0700

    Menambahkan folder WasteNOTT

---
+++ WasteNOTT/First.Domain/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First.Domain.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+ 
+        public required string DateCreated { get; set; }
+        public required string DateShipped { get; set; }
+        public required string CustomerName { get; set; }
+        public required string Status { get; set; }
+        public required bool isPurchased { get; set; }
+        public int ShippingId { get; set; }
+        public required User User { get; set; }
+
+        
+        public void placeOrder() { }
+        public void confirmOrder(Order order) { }
+    }
+}
---
+++ WasteNOTT/First.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First.Domain.Models
+{
+    public class Product
+    {
+        public int id {  get; set; }
+        public required string name { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+    }
+}
